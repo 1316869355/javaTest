aacc1be1fbcbd025591c454e01523bd161736f49
131686935 <1316869355@qq.com>
2017-02-23 15:19:39 +0800

    sort

--- src/main/java/com/up72/sort/insert/InsertSort.java
+++ src/main/java/com/up72/sort/insert/InsertSort.java
@@ -95,6 +95,42 @@ public class InsertSort {
         print(numbers);//打印排序后数组
     }
 
+    public static void exchange(int A[], int i, int j)        // 交换A[i]和A[j]
+    {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    public static int partition(int A[], int left, int right)  // 划分函数
+    {
+        int pivot = A[right];                    // 选择最后一个元素作为基准
+        int tail = left - 1;                     // tail为小于基准的子数组最后一个元素的索引
+        for (int i = left; i < right; i++)       // 遍历基准以外的其他元素
+        {
+            if (A[i] <= pivot)                   // 把小于等于基准的元素放到前一个子数组中
+            {
+                tail++;
+                exchange(A, tail, i);
+            }
+        }
+        exchange(A, tail + 1, right);            // 最后把基准放到前一个子数组的后边,剩下的子数组既是大于基准的子数组
+                                                    // 该操作很有可能把后面元素的稳定性打乱,所以快速排序是不稳定的排序算法
+        return tail + 1;                         // 返回基准的索引
+    }
+
+    public static void quicksort(int A[], int left, int right)
+    {
+        int pivot_index;                        // 基准的索引
+        if (left < right)
+        {
+            pivot_index = partition(A, left, right);
+            quicksort(A, left, pivot_index-1);
+            quicksort(A, pivot_index+1, right);
+        }
+
+    }
+
     /**
      * 打印数组
      *
@@ -106,5 +142,11 @@ public class InsertSort {
             out += object[i] + "\t";
         }
         System.out.println(out);
+    }
+
+    public static void main(String[] args){
+        int A[] = { 5, 2, 9, 4, 7, 6, 1, 3, 8 };// 从小到大快速排序
+        int n = A.length;
+        quicksort(A, 0, n - 1);
     }
 }
